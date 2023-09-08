80137f53924d60744d05802e7a083a5f75470c36
omotayo daniel ukuhor <dani3lsavag3@gmail.com>
2023-09-09 00:21:38 +0100

    implement code splitting

--- src/jsx/App.tsx
+++ src/jsx/App.tsx
@@ -1,10 +1,11 @@
+import { lazy } from 'react';
 
-import Home from './Home/home';
-import Auth from './Auth/auth';
-import Rent from './Rent/index';
-import SingleCar from './SingleCar/index';
-import Layout from './Layout/layout';
-import ComingSoon from './Soon';
+const Home = lazy(()=> import('./Home/home'));
+const Auth = lazy(()=> import('./Auth/auth'));
+const Rent = lazy(()=> import('./Rent/index'));
+const SingleCar = lazy(()=> import('./SingleCar/index'));
+const Layout = lazy(()=> import('./Layout/layout'));
+const ComingSoon = lazy(()=> import('./Soon'));
 
 import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 
